7a5175c9a3a7b9716081771d1341921e8158fb5a
Can Delibas <candelibas96@gmail.com>
2020-06-27 14:51:01 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+build:
+	gcc \
+	-Wfatal-errors \
+	-std=c99 \
+	./src/*.c \
+	-I"C:\libsdl\include" \
+	-L"C:\libsdl\lib" \
+	-lmingw32 \
+	-lSDL2main \
+	-lSDL2 \
+	-o renderer.exe
+
+run:
+	renderer.exe
+
+clean:
+	del renderer.exe
---
+++ src/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include "display.h"
+#include "vector.h"
+
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS];
+
+bool is_running = false;
+
+void setup(void)
+{
+    // Allocate the required bytes in memory for the color buffer
+    color_buffer = (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+
+    // Creating a SDL texture that is used to display the color buffer
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+
+    int point_count = 0;
+    for(float x = -1; x <= 1; x += 0.25) {
+        for(float y = -1; y <= 1; y += 0.25) {
+            for(float z = -1; z <=1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
+}
+
+void process_input(void)
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        is_running = false;
+        break;
+    case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+            is_running = false;
+        break;
+    }
+}
+
+void update(void)
+{
+}
+
+void render(void)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+
+    draw_pixel(20, 20, 0xFFFFFF00);
+
+    render_color_buffer();
+    clear_color_buffer(0xFF000000);
+
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char *args[])
+{
+    is_running = initialize_window();
+
+    setup();
+
+    vec3_t myvec = { 2.0, 3.0, -4.0 };
+
+    while (is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    destroy_window();
+
+    return 0;
+}
