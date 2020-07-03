4230c7d5c8fe0f57ee5ef72c460fd0c0a6938bbd
Can Delibas <candelibas96@gmail.com>
2020-07-03 17:37:22 +0300

    add perspective projection

--- src/main.c
+++ src/main.c
@@ -9,7 +9,9 @@
 vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
 
-float fov_factor = 120;
+vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+
+float fov_factor = 800;
 
 bool is_running = false;
 
@@ -60,8 +62,8 @@ void process_input(void)
 vec2_t project(vec3_t point) // translates 3D view to 2D, don't we have all 2D screens? ;)
 {
     vec2_t projected_point = {
-        .x = (fov_factor * point.x),
-        .y = (fov_factor * point.y),
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z,
     };
 
     return projected_point;
@@ -72,6 +74,8 @@ void update(void)
     for (int i = 0; i < N_POINTS; i++)
     {
         vec3_t point = cube_points[i];
+
+        point.z -= camera_position.z;
 
         vec2_t projected_point = project(point);
 
@@ -91,7 +95,7 @@ void render(void)
             projected_point.y + (window_height / 2),
             4,
             4,
-            0xFFFFFF00
+            0xFF00FF00
         );
     }
 
